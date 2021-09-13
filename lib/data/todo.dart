cc2ee6db7b6d100da65b40fd259cf371014fa9d0
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-09-13 21:35:35 +0900

    TODO 화면 만들기 Step 1

---
+++ lib/data/todo.dart
@@ -0,0 +1,10 @@
+class Todo {
+  String title;
+  String memo;
+  String category;
+  int color;
+  int done;
+  int date;
+
+  Todo({required this.title, required this.memo, required this.category, required this.color, required this.done, required this.date});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/data/todo.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Todo> todos = [
+    Todo(
+      title: 'jyryuitpro 강의듣기1',
+      memo: '앱개발 입문강의 듣기1',
+      category: '공부',
+      color: Colors.redAccent.value,
+      done: 0,
+      date: 20210913,
+    ),
+    Todo(
+      title: 'jyryuitpro 강의듣기2',
+      memo: '앱개발 입문강의 듣기2',
+      category: '공부',
+      color: Colors.blue.value,
+      done: 0,
+      date: 20210913,
+    ),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -40,10 +60,85 @@ class _MyHomePageState extends State<MyHomePage> {
         child: AppBar(),
         preferredSize: Size.fromHeight(0),
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 20,
+              ),
+              child: Text(
+                '오늘 하루',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          } else if (index == 1) {
+            return Container(
+              child: Column(
+                children: List.generate(
+                  todos.length,
+                  (index) {
+                    Todo t = todos[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Color(t.color),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 12,
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 20,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                t.title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                t.done == 0 ? '미완료' : '완료',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 8,
+                          ),
+                          Text(
+                            t.memo,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          } else if (index == 2) {
+          } else if (index == 3) {}
+          return Container();
+        },
+        itemCount: 4,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
